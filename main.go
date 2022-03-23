4d29e29e65761e2bf5d76ddd8e3c47f81bc2721f
xcodesmith <xcodesmith@gmail.com>
2022-03-22 22:19:40 -0400

    add function handlers for go code execution and rerun button

--- main.go
+++ main.go
@@ -34,10 +34,11 @@ func init() {
 }
 
 func main() {
-	var err error
+	// add function handlers for go code execution
 	s.AddHandler(goExecutionHandler)
+	s.AddHandler(goReExec)
 
-	err = s.Open()
+	err := s.Open()
 	if err != nil {
 		log.Fatalf("Cannot open the session: %v", err)
 	}
@@ -52,41 +53,96 @@ func main() {
 func goExecutionHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// handle running code sent from user
 	if c := strings.Split(m.Content, "\n"); c[0] == "run" {
-
-		// check to see if we are executing go code
-		// this is based on a writing standard in discord for writing code in a message
-		// example message: ```go ... ```
-		if strings.Contains(c[1], "go") {
-			// add regex string replacements for content
-			var r []regexp.Regexp
-			runre := regexp.MustCompile("run")
-			blockre := regexp.MustCompile("```.*")
-			whitespacere := regexp.MustCompile("\n\n")
-			r = append(r, *runre, *blockre, *whitespacere)
-
-			// remove strings based on regex for proper code execution
-			content := m.Content
-			for _, regex := range r {
-				content = regex.ReplaceAllString(content, "")
-			}
-
-			// create go execution file
-			ioutil.WriteFile("code/code.go", []byte(content), 0644)
-
-			// run command
-			cmd := exec.Command("go", "run", "code/code.go")
-			o, err := cmd.Output()
-
-			// output error in discord if code did not successfully execute
-			if err != nil {
-				fmt.Println(err.Error())
-				_, _ = s.ChannelMessageSendReply(m.ChannelID, err.Error(), m.Reference())
-				return
-			}
-
-			fmt.Println(string(o))
-			// send execute output
-			_, _ = s.ChannelMessageSendReply(m.ChannelID, string(o), m.Reference())
-		}
+		codeOuput := goExec(m.ChannelID, m.Content, m.Reference())
+		sendMessageComplex(m.ChannelID, m.Reference(), string(codeOuput))
 	}
+}
+
+func goReExec(s *discordgo.Session, m *discordgo.InteractionCreate) {
+	if m.MessageComponentData().CustomID == "go_run" {
+		msg, err := s.ChannelMessage(m.ChannelID, m.Message.MessageReference.MessageID)
+		if err != nil {
+			log.Fatalf("Could not get message reference: %v", err)
+		}
+
+		messageReference := m.Message.Reference()
+		codeOutput := goExec(m.ChannelID, msg.Content, messageReference)
+		editComplexMessage(m.Message.ID, m.ChannelID, messageReference, string(codeOutput))
+	}
+}
+
+func goExec(channelID string, messageContent string, messageReference *discordgo.MessageReference) []byte {
+	// check to see if we are executing go code
+	// this is based on a writing standard in discord for writing code in a message
+	// example message: ```go ... ```
+	if c := strings.Split(messageContent, "\n"); strings.Contains(c[1], "go") {
+		// add regex string replacements for content
+		var r []regexp.Regexp
+		runre := regexp.MustCompile("run")
+		blockre := regexp.MustCompile("```.*")
+		whitespacere := regexp.MustCompile("\n\n")
+		r = append(r, *runre, *blockre, *whitespacere)
+
+		// remove strings based on regex for proper code execution
+		content := messageContent
+		for _, regex := range r {
+			content = regex.ReplaceAllString(content, "")
+		}
+
+		// create go execution file
+		ioutil.WriteFile("code/code.go", []byte(content), 0644)
+
+		// run command
+		cmd := exec.Command("go", "run", "code/code.go")
+		o, err := cmd.Output()
+
+		// output error in discord if code did not successfully execute
+		if err != nil {
+			fmt.Println(err.Error())
+			_, _ = s.ChannelMessageSendReply(channelID, err.Error(), messageReference)
+			return nil
+		}
+
+		return o
+	}
+
+	return nil
+}
+
+func sendMessageComplex(channelID string, messageReference *discordgo.MessageReference, codeOutput string) {
+	_, _ = s.ChannelMessageSendComplex(channelID, &discordgo.MessageSend{
+		Content:   fmt.Sprintf("Output:\n\n`%s`\n", string(codeOutput)),
+		Reference: messageReference,
+		Components: []discordgo.MessageComponent{
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.Button{
+						Label:    "Run",
+						Style:    discordgo.SuccessButton,
+						CustomID: "go_run",
+					},
+				},
+			},
+		},
+	})
+}
+
+func editComplexMessage(messageID string, channelID string, messageReference *discordgo.MessageReference, codeOutput string) {
+	content := fmt.Sprintf("Output:\n\n`%s`\n", string(codeOutput))
+	s.ChannelMessageEditComplex(&discordgo.MessageEdit{
+		ID:      messageID,
+		Channel: channelID,
+		Content: &content,
+		Components: []discordgo.MessageComponent{
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.Button{
+						Label:    "Run",
+						Style:    discordgo.SuccessButton,
+						CustomID: "go_run",
+					},
+				},
+			},
+		},
+	})
 }
